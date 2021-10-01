6697a5f384bc64c45bb228ad9e488ba4bd4efb94
catanielena <catani.elena1@gmail.com>
2021-10-01 15:35:58 +0200

    Add: bootstrap, axios

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -3,8 +3,20 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: "Collection"
+    name: "Collection",
+    data() {
+        // collection: []
+    },
+    mounted() {
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(res => console.log(res.data.response))
+            // .then(res => collection = res.data.response)
+
+    }
 }
 </script>
 
